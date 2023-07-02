ce83e4214a4cd101bcd3109b63ac36d755c1f1f2
Joseph Murray <josephwmurray@gmail.com>
2023-07-02 15:57:40 -0700

    accomplish 1st TDD, gathering a task from user

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,16 @@
 //BL
-
+function Task() {
+  this.task = {};
+}
 
 
 //UI
+
+function handleFormSub(event) {
+  event.preventDefault();
+  const doWhat = document.querySelector("input#new-task").value;
+  console.log(doWhat);
+}
 
 window.addEventListener("load", function() {
   document.querySelector("form#new-task").addEventListener("submit", handleFormSub);
---
+++ readme.md
@@ -0,0 +1,5 @@
+Test 1: "should take a task from the user"
+code:
+const doWhat = "go home"
+function newTask();
+expected output: go home
